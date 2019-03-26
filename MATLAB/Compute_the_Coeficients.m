1d54d1e27fec935889f33372e1d089dea4b5108a
Brusnicki <rbrusnicki@gmail.com>
2019-03-26 18:46:06 -0300

    Delete Compute_the_Coeficients.m

--- MATLAB/Compute_the_Coeficients.m
+++
@@ -1,9 +0,0 @@
-function [] = Compute_the_Coeficients(Airflow_speed, Temperature)
-
-K = 1.0/340;
-
-Mach = K * sqrt(Temperature);
-
-
-load('Roll Drive.txt');
-vento_1(:,1) = interp1(VENTO_1(:,1),VENTO_1(:,2),tempo,'spline');
